350c75d1792f1dff3fdd584925b207968721e685
Gwendall Garnier <gwendall@MacBook-Pro-de-Gwendall.local>
2020-01-23 14:53:38 +0100

    add test

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+provider "aws" {
+    region = "eu-west-1"
+}
+
 output "test" {
   value = "my test"
+}
+
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = "${data.aws_caller_identity.current.account_id}"
 }
