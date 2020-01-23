2795a4acd5fef27da3611823b3dc3158e80a5b34
Gwendall Garnier <gwendall@MacBook-Pro-de-Gwendall.local>
2020-01-23 21:11:07 +0100

    test terragrunt

---
+++ non-prod/stage/eu-west-1/s3/.terragrunt-cache/kmVmW73vqJOg0LNRCfhaWqVgnmc/cPp82P-2ALS_IWkQe4q6OLZv7po/variables.tf
@@ -0,0 +1,14 @@
+variable "bucket_name" {
+    description = "Name of bucket"
+    type = string
+}
+
+variable "aws_region" {
+  description = "The aws region to deploy"
+  type        = string
+}
+
+variable "environment" {
+    description = "Environment of deployment"
+    type = string
+}
