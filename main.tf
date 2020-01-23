6476383745d53abd3c76a174359cc4c3ed2fdf19
Gwendall Garnier <gwendall@MacBook-Pro-de-Gwendall.local>
2020-01-23 16:38:07 +0100

    update

--- main.tf
+++ main.tf
@@ -3,6 +3,12 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = "${data.aws_caller_identity.current.account_id}"
+}
+
 output "test" {
   value = "my test"
 }
